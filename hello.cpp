a2a2e6aaa6674afd737a5bdb369fd95e7cadda26
niconii <nico.huber@bulme.at>
2021-10-01 13:05:32 +0200

    AddedNameInput

--- hello.cpp
+++ hello.cpp
@@ -5,6 +5,11 @@ using namespace std;
 
 int main()
 {
+	string name;
 	cout << "Hallo Lieber Lehrer!" << endl;
+	cout << "Wie heisst du? ";
+	cin >> name;
+	cout << "Hallo " << name << endl;
+	
 	return 0;
 }
